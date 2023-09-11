2ea2141f935743e2d47b388d78bc3b1c418150f9
yasircs <yasir.arfatcs@gmail.com>
2023-09-11 12:57:30 +0500

    completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -43,7 +43,7 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
-            <h1 className='mb-4'>{props.heading}</h1>
+            <h3 className='mb-4'>{props.heading}</h3>
             <div className="mb-3"> 
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
@@ -54,7 +54,7 @@ export default function TextForm(props) {
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
-            <h2>Your text summary</h2>
+            <h3>Your text summary</h3>
             <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
